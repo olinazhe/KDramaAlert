fefa6a869d669b2842797feea6f6ceb0157de2fd
Olina Zheng <olinazheng@dhcp-vl2042-14965.redrover.cornell.edu>
2025-03-19 13:59:20 -0400

    updated json file to have the genres represented as a list

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,19 @@ from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 import pandas as pd
 
+#change string representation of genres to list
+def genre_to_array():
+    for idx,row in kdramas_df.iterrows():
+        genre = row['genre']
+        if isinstance(genre, list):
+            list_of_strings = genre
+        else: 
+            list_of_strings = genre.split(",")
+        new_list = []
+        for s in list_of_strings:
+            new_list.append(s.strip())
+        kdramas_df.at[idx, 'genre'] = new_list
+
 # ROOT_PATH for linking with all your files. 
 # Feel free to use a config.py or settings.py with a global export variable
 os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
@@ -19,6 +32,9 @@ json_file_path = os.path.join(current_directory, 'init.json')
 with open(json_file_path, 'r') as file:
     data = json.load(file)
     kdramas_df = pd.DataFrame(data)
+    genre_to_array()
+with open(json_file_path, 'w') as file:
+    json.dump(kdramas_df.to_dict(orient='records'), file, indent=2)
 
 app = Flask(__name__)
 CORS(app)
@@ -42,3 +58,7 @@ def episodes_search():
 
 if 'DB_NAME' not in os.environ:
     app.run(debug=True,host="0.0.0.0",port=5000)
+
+
+       
+
