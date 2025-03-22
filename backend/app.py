20c7541c37cd53feb2952b1ce3f6ba12719b8cf7
Chris Chen <144409850+cchrischen@users.noreply.github.com>
2025-03-21 20:02:44 -0400

    change display of result

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,6 @@ import json
 import os
 from flask import Flask, render_template, request
 from flask_cors import CORS
-from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 import pandas as pd
 
 # ROOT_PATH for linking with all your files. 
@@ -26,14 +25,19 @@ CORS(app)
 # Sample search using json with pandas
 def json_search(query):
     synopsis_matches, title_matches = [], []
-    synopsis_matches = kdramas_df[kdramas_df['synopsis'].str.lower().str.contains(query.lower())]
-    title_matches = kdramas_df[kdramas_df['name'].str.lower().str.contains(query.lower())]
+    query = query.split(" ")
+    for word in query:
+        synopsis_matches.append(kdramas_df[kdramas_df['synopsis'].str.lower().str.contains(word.lower())])
+        title_matches.append(kdramas_df[kdramas_df['name'].str.lower().str.contains(word.lower())])
+
+    synopsis_matches = pd.concat(synopsis_matches).drop_duplicates(keep="first")
+    title_matches = pd.concat(title_matches).drop_duplicates(keep="first")
 
     matches = pd.concat([title_matches, synopsis_matches]).drop_duplicates(keep="first")
 
-    matches_filtered = matches[['name', 'synopsis', 'score']]
+    matches_filtered = matches
     if matches_filtered.empty:
-        matches_filtered = kdramas_df[['name','synopsis','score']]
+        matches_filtered = kdramas_df
     matches_filtered_json = matches_filtered.to_json(orient='records')
     return matches_filtered_json
 
