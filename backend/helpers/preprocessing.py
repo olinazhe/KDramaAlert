9df8d214956dae2bdeba479371a03ea3ae30ca7f
Ethan Huang <ejhuang5@gmail.com>
2025-04-13 19:03:01 -0400

    update

--- backend/helpers/preprocessing.py
+++ backend/helpers/preprocessing.py
@@ -2,6 +2,34 @@ import re
 import numpy as np
 from typing import List, Tuple
 from sklearn.feature_extraction.text import TfidfVectorizer
+from collections import defaultdict
+import math
+import pandas as pd
+from pandas import DataFrame
+
+def process_data(data):
+  kdramas_df = pd.DataFrame(data)
+  #tags spit differently
+  convert = ['genres','network', 'tags', 'mainCast']
+  for t in convert:
+    if t == 'tags':
+      for idx,row in kdramas_df.iterrows():
+        tags = row[t]
+        list_of_strings = tags.split(",, ")
+        new_list = []
+        for s in list_of_strings:
+            new_list.append(s.strip())
+        kdramas_df.at[idx,t] = new_list
+    else:
+      for idx,row in kdramas_df.iterrows():
+        genre = row[t]
+        list_of_strings = genre.split(", ")
+        new_list = []
+        for s in list_of_strings:
+          new_list.append(s.strip())
+        kdramas_df.at[idx,t] = new_list
+  return kdramas_df
+
 
 def strip_text(text: str, regex: str = r"\w+(?:'\w+)?") -> List[str]:
   """
@@ -17,11 +45,57 @@ def _build_vectorizer(max_features, stop_words, max_df=0.8, min_df=10, norm='l2'
   
   return TfidfVectorizer(stop_words=stop_words, max_features=max_features, max_df=max_df, min_df=min_df, norm=norm)
 
-def build_td_mat(documents: str) -> Tuple[np.ndarray, np.ndarray]:
+def build_td_mat(dataframe: DataFrame) -> Tuple[np.ndarray, dict]:
   """
   Returns the term document matrix along with the terms that represent each column.
   """
   vectorizer = _build_vectorizer(5000, "english")
-  X = vectorizer.fit_transform(documents).toarray()
-  terms = vectorizer.get_feature_names_out()
-  return X, terms
+  doc_to_vocab = vectorizer.fit_transform(dataframe["synopsis"].to_list()).toarray()
+  index_to_vocab = list(vectorizer.get_feature_names_out())
+  return doc_to_vocab, index_to_vocab
+
+def build_inverted_index(msgs: List[str]) -> dict:
+    """
+    Builds an inverted index .
+    """
+    result = defaultdict(list)
+    for i, msg in enumerate(msgs):
+      counts = defaultdict(int)
+      tokenized_msg = msg.split(" ")
+      for token in tokenized_msg:
+        counts[token] += 1
+      for term, count in counts.items():
+        result[term].append((i, count))
+    return result
+
+def compute_idf(inv_idx, n_docs, min_df=10, max_df_ratio=0.95):
+    """Compute term IDF values from the inverted index.
+    Words that are too frequent or too infrequent get pruned.
+    """
+    result = {}
+    for term in inv_idx:
+      df = len(inv_idx[term])
+      if (df >= min_df and (df / n_docs) <= max_df_ratio):
+        result[term] = math.log2(n_docs / (1 + df))
+    return result
+
+def compute_doc_norms(index, idf, n_docs):
+    """
+    Precompute the euclidean norm of each document.
+    """
+    result = np.zeros(n_docs)
+    for term in index:
+      if term in idf:
+        value = idf[term]
+        for doc, count in index[term]:
+          result[doc] += (count * value) ** 2
+    return np.sqrt(result)
+
+def drama_name_to_index(docs_df):
+  """
+  Returns a dictionary with keys as drama names and values as their index in the data frame.
+  """
+  res = {}
+  for index, row in docs_df.iterrows():
+    res[row["name"]] = index
+  return res
--- backend/helpers/similarity.py
+++ backend/helpers/similarity.py
@@ -1,10 +1,64 @@
 import numpy as np
 from typing import List
+from collections import defaultdict
 
-def get_sim(q, term_doc_matrix, tfidf):
-    """Returns cosine similarity
+def query_word_counts(query):
+    query = query.lower().split(" ")
+    query_dictionary = defaultdict(int)
+    for word in query:
+      query_dictionary[word] += 1
+    return query_dictionary
+
+def doc_scores(query_word_counts: dict, inverted_index: dict, idf: dict) -> dict:
     """
-    similarities = []
-    for d in term_doc_matrix:
-        similarities.append(np.dot(q, d) / (np.linalg.norm(q) * np.linalg.norm(d)))
-    return similarities
+    Perform a term-at-a-time iteration to efficiently compute the numerator term of cosine similarity across multiple documents.
+    """
+    result = defaultdict(float)
+    for word in query_word_counts:
+      if word in inverted_index and word in idf:
+        query_weight = query_word_counts[word] * idf[word]
+        for doc, count in inverted_index[word]:
+          doc_weight = count * idf[word]
+          result[doc] += query_weight * doc_weight
+    return result 
+
+
+def index_search(query: str, inverted_index: dict, idf, doc_norms):
+    """
+    Search the collection of documents for the given query
+    """
+    query_tokens = query.lower().split(" ")
+
+    tf= defaultdict(int)
+    for token in query_tokens:
+        tf[token] += 1
+
+    query_norm = 0
+    for word in tf:
+      if word in idf:
+        word_idf = idf.get(word, 0)
+        query_norm += (tf[word] * word_idf) ** 2
+    query_norm = np.sqrt(query_norm)
+
+    cossim = doc_scores(tf, inverted_index, idf)
+
+    results = []
+    for i in cossim:
+      if query_norm > 0 and doc_norms[i] > 0:
+        new_score = cossim[i] / (query_norm * doc_norms[i])
+        results.append((new_score, i))
+
+    results.sort(key = lambda x : x[0], reverse = True)
+    return results
+
+def get_title_sim(query: str, titles: List[str]) -> np.ndarray:
+    """
+    Returns the similarity between a query and each title using Jaccard similarity in the range [0, 1].
+    """
+
+    types = set(query.split(" "))
+    result = np.zeros((len(titles),))
+    for i, title in enumerate(titles):
+        title_tokens = set(title.split(" "))
+        result[i] = len(title_tokens.intersection(types)) / len(title_tokens)
+    return result
