2d98d823620ffc653d43b7001716a45019ea3a13
Chris Chen <144409850+cchrischen@users.noreply.github.com>
2025-04-25 18:11:46 -0400

    yolo

--- backend/helpers/similarity.py
+++ backend/helpers/similarity.py
@@ -203,14 +203,29 @@ def get_drama_details(id, df: pd.DataFrame, td_matrix: np.ndarray, docs_compress
     similar_dramas = [[genre, get_top_dramas_by_genre(df, genre, id, td_matrix)] for genre in initial_details["genres"]]
     initial_details["similarDramas"] = similar_dramas
 
-
+    index_to_index = {
+        0: 1,
+        1: 2,
+        2: 3,
+        3: 4,
+        4: 5,
+        5: 6,
+        6: 8,
+        7: 11,
+        8: 12,
+        9: 13,
+        10: 16,
+        11: 17,
+        12: 18,
+        13: 19
+        }
     valid_dims = [1,2,3,4,5,6,8,11,12,13,16,17,18,19]
     query_vec = docs_compressed[int(id),][valid_dims]
     indices = np.argsort(query_vec)[-3:][::-1]
 
     word_to_index = vectorizer.vocabulary_    
     index_to_word = {i:t for t,i in word_to_index.items()}
-    list_of_asorts = [ np.argsort(-words_compressed[:,index].squeeze()) for index in indices]
+    list_of_asorts = [ np.argsort(-words_compressed[:,index_to_index[index]].squeeze()) for index in indices]
     latent_words = [ [index_to_word[i] for i in asort[:20]] for asort in list_of_asorts]
 
     index_to_genre = {0: "School", 1: "Romance", 2: "Family", 3:"Historical", 4: "Drama", 5: "Thriller", 6: "Life", 7: 'Chaebol', 8:"Medical", 9:"Friends", 10: "Law", 11:"Daughter/Mother", 12:"College", 13:"Father/Son"}
